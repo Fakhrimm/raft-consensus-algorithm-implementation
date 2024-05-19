f92d0bff0c3c584b6a7ce6858e03f08eda757abd
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2024-05-19 19:07:22 +0700

    feat: ip address detection

--- start.ps1
+++ start.ps1
@@ -6,41 +6,54 @@ function BuildProto {
 
 function StartServer {
     param (
-        [string]$Addr = "0.0.0.0:60000",
+        [string]$Addr,
+        [int]$Port = 60000,
         [int]$Timeout = 5
     )
-    Write-Host "Starting node on $Addr"
-    Start-Process -FilePath "cmd" -ArgumentList "/c start cmd /k `"go run src/main.go -addr $Addr -timeout $Timeout`"" -NoNewWindow
+    if(-not $Addr) {
+        $Addr = Get-WiFiIPAddress
+        if (-not $Addr) {
+            $Addr = "0.0.0.0"
+        }
+    }
+
+    Write-Host "Starting node on ${Addr}:${Port}"
+    Start-Process -FilePath "cmd" -ArgumentList "/c start cmd /k `"go run src/main.go -addr ${Addr}:${Port} -timeout $Timeout`"" -NoNewWindow
 }
 
 function StartServers {
     param (
         [int]$Size = 1,
-        [string]$Addr = "0.0.0.0",
+        [string]$Addr,
         [int]$Port = 60000,
         [int]$Timeout = 5,
         [string]$Hostfile = "./config/Hostfile"
     )
+    if (-not $Addr) {
+        $Addr = Get-WiFiIPAddress
+        if (-not $Addr) {
+            $Addr = "0.0.0.0"
+        }
+    }
 
     Write-Host "Generating hostfile"
     Remove-Item -Path $Hostfile -Recurse -Force
     
     for ($i = 0; $i -lt $Size; $i++) {
         $nPort = $Port + $i
-        $address = "0.0.0.0:$nPort"
+        $address = "${Addr}:${nPort}"
         $address | Out-File -FilePath $Hostfile -Append -Encoding ascii
     }
 
     Write-Host "Starting $Size servers"
     for ($i = 0; $i -lt $Size; $i++) {
         $nPort = $Port + $i
-        $nAddr = "${Addr}:${nPort}"
-        StartServer -Addr $nAddr -Timeout $Timeout -Hostfile $Hostfile
+        StartServer -Addr $nAddr -Port $nPort -Timeout $Timeout -Hostfile $Hostfile
     }   
 }
 
 function Get-WiFiIPAddress {
-    $wifiInterface = Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "*Wireless*"}
-    $wifiIPAddress = Get-NetIPAddress -InterfaceIndex $wifiInterface.ifIndex | Where-Object {$_.AddressFamily -eq "IPv4"} | Select-Object -ExpandProperty IPAddress
-    return $wifiIPAddress
+    $wifiIPAddress = Get-NetIPAddress | Where-Object -FilterScript {$_.InterfaceAlias -Eq "Wi-Fi" -and $_.AddressFamily -Eq "IPv4"}
+    $ipAddress = $wifiIPAddress.IPAddress
+    return $ipAddress
 }
