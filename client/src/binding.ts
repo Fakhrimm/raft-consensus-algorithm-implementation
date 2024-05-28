809cfd9342934b2196cdc86f56ae17203ff76b4e
razzanYoni <13521087@mahasiswa.itb.ac.id>
2024-05-28 18:58:38 +0700

    feat: binding ui

---
+++ client/src/binding.ts
@@ -0,0 +1,40 @@
+// ui binding
+const pingButton = document.getElementById("ping-button") as HTMLButtonElement;
+
+const getInput = document.getElementById("get-input") as HTMLInputElement;
+const getButton = document.getElementById("get-button") as HTMLButtonElement;
+
+const setKeyInput = document.getElementById("set-key-input") as HTMLInputElement;
+const setValueInput = document.getElementById("set-value-input") as HTMLInputElement;
+const setButton = document.getElementById("set-button") as HTMLButtonElement;
+
+const strlenInput = document.getElementById("strlen-input") as HTMLInputElement;
+const strlenButton = document.getElementById("strlen-button") as HTMLButtonElement;
+
+const delInput = document.getElementById("del-input") as HTMLInputElement;
+const delButton = document.getElementById("del-button") as HTMLButtonElement;
+
+const appendKeyInput = document.getElementById("append-key-input") as HTMLInputElement;
+const appendValueInput = document.getElementById("append-value-input") as HTMLInputElement;
+const appendButton = document.getElementById("append-button") as HTMLButtonElement;
+
+const messageInput = document.getElementById("message-input") as HTMLInputElement;
+const sendButton = document.getElementById("send-button") as HTMLButtonElement;
+
+export {
+  pingButton,
+  getInput,
+  getButton,
+  setKeyInput,
+  setValueInput,
+  setButton,
+  strlenInput,
+  strlenButton,
+  delInput,
+  delButton,
+  appendKeyInput,
+  appendValueInput,
+  appendButton,
+  messageInput,
+  sendButton
+}
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,2 +1,63 @@
 import "./style.css";
-console.log("UI Set up");
+import { CommServiceClient } from "./grpc/comm.client";
+
+import {
+  BasicRequest,
+} from "./grpc/comm";
+import {
+  pingButton,
+  getButton,
+  setButton,
+  strlenButton,
+  delButton,
+  appendButton,
+  sendButton
+} from "./binding.ts";
+import { GrpcWebFetchTransport } from "@protobuf-ts/grpcweb-transport";
+
+
+// grpc
+const transport = new GrpcWebFetchTransport({
+  baseUrl: "/10.1.78.161:60000/",
+  format: "binary",
+});
+
+const client = new CommServiceClient(
+  transport
+);
+
+// event binding
+pingButton.onclick = async () => {
+  const request = BasicRequest.create({ value: "ping" });
+
+  try {
+    const response = await client.ping(request);
+    console.log(response);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getButton.onclick = async () => {
+  // TODO
+}
+
+setButton.onclick = async () => {
+  // TODO
+}
+
+strlenButton.onclick = async () => {
+  // TODO
+}
+
+delButton.onclick = async () => {
+  // TODO
+}
+
+appendButton.onclick = async () => {
+  // TODO
+}
+
+sendButton.onclick = async () => {
+  // TODO
+}
