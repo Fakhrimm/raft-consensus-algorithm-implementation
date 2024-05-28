02abf78f98b052fb867491018066ed5d2518564d
razzanYoni <13521087@mahasiswa.itb.ac.id>
2024-05-28 22:07:20 +0700

    comment grpc web on golang

--- node/main.go
+++ node/main.go
@@ -3,13 +3,6 @@ package main
 import (
 	node "Node/lib"
 	"flag"
-	"fmt"
-	"github.com/improbable-eng/grpc-web/go/grpcweb"
-	"google.golang.org/grpc"
-	"log"
-	"net/http"
-
-	pb "Node/grpc/comm"
 )
 
 func main() {
@@ -26,29 +19,29 @@ func main() {
 	flag.BoolVar(&isJointConsensus, "isjoinconsensus", false, "The list of new servers in the cluster")
 	flag.Parse()
 
-	var service pb.CommServiceServer
-	grpcServer := grpc.NewServer()
-	pb.RegisterCommServiceServer(
-		grpcServer,
-		service,
-	)
+	//var service pb.CommServiceServer
+	//grpcServer := grpc.NewServer()
+	//pb.RegisterCommServiceServer(
+	//	grpcServer,
+	//	service,
+	//)
 
 	node := node.NewNode(addr)
 	node.Init(".."+hostfile, timeoutAvg, ".."+hostfileNew, isJointConsensus)
 
-	wrappedGrpc := grpcweb.WrapServer(grpcServer)
-	httpServer := &http.Server{
-		Addr: ":8080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.ProtoMajor == 2 {
-				wrappedGrpc.ServeHTTP(w, r)
-			} else {
-				http.Error(w, "Only gRPC requests are accepted", http.StatusNotImplemented)
-			}
-		}),
-	}
-	fmt.Println("Starting server on port 8080")
-	log.Fatal(httpServer.ListenAndServe())
+	//wrappedGrpc := grpcweb.WrapServer(grpcServer)
+	//httpServer := &http.Server{
+	//	Addr: ":8080",
+	//	Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	//		if r.ProtoMajor == 2 {
+	//			wrappedGrpc.ServeHTTP(w, r)
+	//		} else {
+	//			http.Error(w, "Only gRPC requests are accepted", http.StatusNotImplemented)
+	//		}
+	//	}),
+	//}
+	//fmt.Println("Starting server on port 8080")
+	//log.Fatal(httpServer.ListenAndServe())
 
 	for node.Running {
 	}
