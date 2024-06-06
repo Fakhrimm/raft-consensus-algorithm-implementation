974953850334e7bf83c19159d5edab3dac98b636
Fakhrimm <13521045@mahasiswa.itb.ac.id>
2024-06-06 13:41:25 +0700

    docs: for debugging purposes

--- node/main.go
+++ node/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	node "Node/lib"
 	"flag"
+	"log"
 )
 
 func main() {
@@ -16,10 +17,13 @@ func main() {
 	flag.IntVar(&timeoutAvg, "timeout", 200, "The average time before a node declares a timeout")
 	flag.StringVar(&hostfile, "hostfile", "/config/Hostfile", "The list of all servers in the cluster")
 	flag.StringVar(&hostfileNew, "hostfilenew", "", "The list of new servers in the cluster")
-	flag.BoolVar(&isJointConsensus, "isjoinconsensus", false, "The list of new servers in the cluster")
+	flag.BoolVar(&isJointConsensus, "isjointconsensus", false, "The list of new servers in the cluster")
 	flag.Parse()
 
 	node := node.NewNode(addr)
+	log.Printf("hostFile: %v", ".."+hostfile)
+	log.Printf("hostFileNew: %v", ".."+hostfileNew)
+	log.Printf("isJointConsensus: %v", isJointConsensus)
 	node.Init(".."+hostfile, timeoutAvg, ".."+hostfileNew, isJointConsensus)
 
 	for node.Running {
